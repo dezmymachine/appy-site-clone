1549d3d9a562868dcccdd68bef2ee749e6f87a98
= <desomondkwakucoffie195@gmail.com>
2024-02-12 13:54:34 -0800

    HTML structure and Navbar

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log("we are beta")
+var firstName="desmond";
+var age = 63;
+
+let lastName = "money";
+const tempBoil= 300;
+
+console.log(firstName,lastName,age,tempBoil)
+if (age===18){
+    console.log("You are eligible")
+}
+//Loops
+for(let i=0; i<8; i++){
+    console.log("I am at index", i)
+}
+
+//functions
+let p;
+function isEighteen(){
+    if(p >=18){
+        console.log("feel free to enter")
+    }
+    else{
+        console.log("get out!")
+    }
+}
+
+isEighteen(60)
